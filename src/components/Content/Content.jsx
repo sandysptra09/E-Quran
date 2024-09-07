6a2ffd74c65518368fe1b40afbcc5111a4fd291c
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2024-09-07 09:37:58 +0700

    Update

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -77,6 +77,7 @@ export default function DetailSurah({ surahNomor}) {
                                         {detailSurah.nomor}
                                     </span>
                                 </div>
+                                
                             </div>
                         </div>
                         <div className="flex gap-3 py-4">
